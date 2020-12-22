567bf28286e0ce2b7831708717c32caeeee073f7
Sarah Eaglesfield <speedbunny@gmail.com>
2020-12-22 02:38:20 +0000

    Update games-howell.py

--- games-howell.py
+++ games-howell.py
@@ -1,20 +1,26 @@
+filepath = '/users/apple/Datasets/PA/PAt.txt'
+
+#Import Default Dict
 from collections import defaultdict
 
+#Declare dictionary and all
 d = defaultdict(list)
 all = []
 
+#Read file line by line for each line
 with open(filepath) as f:
     for line in f:
         if line.strip():
+        #Create three variables
             k,v,p = map(float,line.split(","))
-            if (p > 0.9):
-             if v not in all or k not in all: 
+            #If p is 1
+            if (p == 1):
+            #Uncomment to just return single pairs
+             #if v not in all or k not in all: 
+             #Add value v to dictionary with key k
                  d[k].append(v)
-                 all.append (v)
+              #   all.append (k)
+              #   all.append (k)
 for key,values in d.items():
  if values:
   print(key, values)
-
-
-l = [ [k]+v for k,v in d.items()]
-
